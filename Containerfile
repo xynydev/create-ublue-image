41719a5f0046e19f388f6613127ef23ebfaf853f
ER <eino.rauhala@proton.me>
2023-03-13 19:48:24 +0200

    feat: basic repo cloning

--- Containerfile
+++ Containerfile
@@ -3,6 +3,7 @@ FROM alpine:3.17
 COPY wizard.sh /usr/bin/wizard.sh
 
 RUN chmod +x /usr/bin/wizard.sh
-RUN apk add gum
+RUN apk update
+RUN apk add git openssh gum github-cli
 
 ENTRYPOINT ["/usr/bin/wizard.sh"]
--- wizard.sh
+++ wizard.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
-echo "test"
-gum choose "abc" "xyz"
+echo "--- Welcome to the uBlue image creation wizard! --- "
+
+cd /host
+
+echo "To get started, please log in to Github. This is used to set up the repository, nothing will be deleted or modified, only added!"
+gh auth login
+
+REPO_DIR=$(gum input --placeholder "Directory name for your custom image repository.")
+
+gh repo clone ublue-os/startingpoint $REPO_DIR
