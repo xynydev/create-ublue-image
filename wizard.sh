2c0c84206eea87bb8aa1c8c063cb41c54b23bdfc
ER <eino.rauhala@proton.me>
2023-03-13 20:01:34 +0200

    feat: mvp this now creates a functioning repo based on user inputs

--- wizard.sh
+++ wizard.sh
@@ -4,8 +4,16 @@ echo "--- Welcome to the uBlue image creation wizard! --- "
 cd /host
 
 echo "To get started, please log in to Github. This is used to set up the repository, nothing will be deleted or modified, only added!"
-gh auth login
+gh auth login -p https -w
 
+echo ""
 REPO_DIR=$(gum input --placeholder "Directory name for your custom image repository.")
-
 gh repo clone ublue-os/startingpoint $REPO_DIR
+
+cd $REPO_DIR
+git remote rm upstream
+git remote rename origin upstream
+
+echo ""
+REPO_NAME=$(gum input --placeholder "Public repository name on Github for your custom image repository.")
+gh repo create $REPO_NAME --source . --push --public 
